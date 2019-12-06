3087fd2ebcc974f00c2ef3df13c221d865e2ba05
Ashley Yang (RD-TW) <ashley_yang@trendmicro.com>
2019-12-07 03:10:19 +0800

    Updates

---
+++ precache-manifest.2f1d0f74f9c1c91e22c0148baf828001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb9ef59922c845d06ea51ec6edfc52b",
+    "url": "/Infinite-scroll/index.html"
+  },
+  {
+    "revision": "5c3d6d6c6c7d33002eaf",
+    "url": "/Infinite-scroll/static/css/main.910cdd16.chunk.css"
+  },
+  {
+    "revision": "8fc27d3f5fc9d154fc63",
+    "url": "/Infinite-scroll/static/js/2.bf37d767.chunk.js"
+  },
+  {
+    "revision": "5c3d6d6c6c7d33002eaf",
+    "url": "/Infinite-scroll/static/js/main.94a74d71.chunk.js"
+  },
+  {
+    "revision": "d0e18570647a9dea39ef",
+    "url": "/Infinite-scroll/static/js/runtime-main.f85f543b.js"
+  }
+]);
