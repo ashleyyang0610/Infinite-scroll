ec424b53dce472e6b0cf851b0499884831f3e8c5
Ashley Yang (RD-TW) <ashley_yang@trendmicro.com>
2019-12-09 18:51:46 +0800

    Modify loader component.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
-const Loader = ({ ...props }) => {
-    return (
-        <div {...props}>
-            <FontAwesomeIcon
-                icon={faCircleNotch}
-                size="3x"
-                spin
-                style={{
-                    color: '#ced4da',
-                    ...props.style
-                }}
-            />
-        </div>
-    );
+const StyledLoader = styled.div`
+    margin-top: -22px;
+    font-size: 10px;
+    position: relative;
+    text-indent: -9999em;
+    border-top: 2px solid rgba(255, 255, 255, 0.2);
+    border-right: 2px solid rgba(255, 255, 255, 0.2);
+    border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+    border-left: 2px solid #555;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: load8 1.1s infinite linear;
+    animation: load8 1.1s infinite linear;
+
+    &,
+    &:after {
+        border-radius: 50%;
+        width: 44px;
+        height: 44px;
+    }
+    @-webkit-keyframes load8 {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes load8 {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+`;
+
+const Loader = ({ className, ...props }) => {
+    return <StyledLoader className={className} />;
 };
 
 export default Loader;
